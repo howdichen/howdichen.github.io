b701f906bfa227bbe5bc4d0ee78d462645183ad5
howdi <t13243334@hotmail.com>
2022-10-25 16:58:47 +0800

    update google行事曆

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ function removeActiveClasses() {
     panel.classList.remove("active");
   });
 }
+
+const day = document.getElementById("dayleft");
+day.innerText = dayleft();
+
+function dayleft() {
+  var now = new Date();
+  var spday = new Date(2023, 1 - 1, 14); //js 從第0月開始
+  a = (spday.getTime() - now.getTime()) / (24 * 60 * 60 * 1000);
+  a = Math.ceil(a);
+
+  return `還有${a}天`;
+}
