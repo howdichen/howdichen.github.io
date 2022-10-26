d99e0f71f43999415124025d215252e80bd894dd
howdi <t13243334@hotmail.com>
2022-10-26 19:22:24 +0800

    update nav

--- script.js
+++ script.js
@@ -24,6 +24,22 @@ function dayleft() {
   var spday = new Date(2023, 1 - 1, 14); //js 從第0月開始
   a = (spday.getTime() - now.getTime()) / (24 * 60 * 60 * 1000);
   a = Math.ceil(a);
+  if (a == 0) {
+    return "就在今天! 歡迎蒞臨";
+  } else if (a < 0) {
+    return "完美落幕，感謝參與!";
+  }
 
   return `還有${a}天`;
+}
+
+const nav = document.querySelector(".nav");
+window.addEventListener("scroll", fixNav);
+
+function fixNav() {
+  if (window.scrollY > nav.offsetHeight + 150) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
 }
